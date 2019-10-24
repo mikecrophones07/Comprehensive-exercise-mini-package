fa427bc9a59cca1ecb2438bb17fe43ce216883e7
Mike Anthony A. Echon <mike.toradora07@gmail.com>
2019-10-24 09:58:05 +0800

    initial Commit

---
+++ mini-package/src/test/java/com/minipackage/minipackage/MiniPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minipackage.minipackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
